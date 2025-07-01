a580908b218d825a66bccbdc68446a5a0b8d7fc8
Tuwane <152686997+Tuwane@users.noreply.github.com>
2025-07-01 23:49:56 +0200

    adding area and rectangular files

---
+++ rectangular_cal/are_rec_calcu.py
@@ -0,0 +1,15 @@
+import line_1
+
+length = float(input("Enter the Value of the length in Area"))
+width = float(input("Enter the Value of the width in area"))
+
+area = line_1.area(length, width)
+rect = line_1.rect(length , width)
+
+
+
+
+
+
+print(f"The Area of the triangle is {area}")
+print(f"The Area of reactangulor is {rect}")
